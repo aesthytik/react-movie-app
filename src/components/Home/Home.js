baba95dde9fe8da74cd0ef8ade82e1f5db0c0c1c
aesthytik <rawat.vipin4818@gmail.com>
2019-01-25 03:01:55 +0530

    updated home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,42 @@
 import React from 'react';
+import { API_URL, API_KEY } from '../../config';
 
-export default (params) => (
-  <div>Home</div>
-)
+class Home extends React.Component {
+  state = {
+    movies: [],
+    heroImage: null,
+    loading: false,
+    currentPage: 0,
+    totalPages: 0,
+    searchTerm: '',
+  };
+
+  componentDidMount = () => {
+    this.setState({ loading: true });
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    this.fetchItems(endpoint);
+  };
+
+  fetchItems = endpoint => {
+    const { movies, heroImage } = this.state;
+    fetch(endpoint)
+      .then(result => result.json())
+      .then(
+        result =>
+          this.setState({
+            movies: [...movies, ...result.results],
+            heroImage: heroImage || result.results[0],
+            loading: false,
+            currentPage: result.page,
+            totalPages: result.total_pages,
+          })
+        // console.log(result)
+      );
+  };
+
+  render() {
+    return <div>Home</div>;
+  }
+}
+
+export default Home;
